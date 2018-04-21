52323f4d3e72065ce33c7886ec6b43a92790684f
Santosh Marigowda <santosharakere@gmail.com>
2018-04-21 18:40:38 +0100

    GAL-2-form-input (#2)

--- src/App.js
+++ src/App.js
@@ -2,17 +2,43 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
+  constructor() {
+    super();
+    
+    this.state = {
+      gallery: 'start', // finite state
+      query: '',
+      items: []
+    };
+  }
+
+  renderForm(state) {
+    const searchText = {
+      loading: 'Searching...',
+      error: 'Try search again',
+      start: 'Search'
+    }[state] || 'Search';
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <form onSubmit={e => this.handleSubmit(e)}>
+        <input
+          type="search"
+          className="ui-input"
+          value={this.state.query}
+          onChange={e => this.handleChangeQuery(e.target.value)}
+          placeholder="Search Flickr for photos..."
+          disabled={state === 'loading'}  
+        />
+      </form>
+    )
+  }
+
+  render() {
+    const galleryState = this.state.gallery;
+    return (
+      <div className="ui-app" data-state={galleryState}>
+        {this.renderForm(galleryState)}
       </div>
-    );
+    )
   }
 }
 
